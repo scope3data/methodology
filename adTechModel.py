4ce0d74b923f241af4da53c5e6ccc1d4dbee7705
Brian O'Kelley <bokelley@scope3.com>
2022-08-23 06:18:01 +0100

    fix units on output, add verbose mode

--- adTechModel.py
+++ adTechModel.py
@@ -11,8 +11,17 @@ parser.add_argument(
     default="defaults.yaml",
     help="Set the defaults file to use (overrides defaults.yaml)",
 )
+parser.add_argument("-v", "--verbose", action="store_true", help="Show derivation of output")
 parser.add_argument("companyFile", nargs=1, help="The company file to parse in YAML format")
 args = parser.parse_args()
+
+if args.verbose:
+
+    def verboseprint(*args, **kwargs):
+        print(*args, **kwargs)
+
+else:
+    verboseprint = lambda *a, **k: None  # do-nothing function
 
 # Load defaults
 defaultsStream = open(args.defaultsFile, "r")
@@ -36,46 +45,58 @@ for document in documents:
                 facts[key] = fact["fact"][key]
 
 
-def getFactOrDefault(key: str, facts: Dict[str, float], defaults: Dict[str, float]) -> float:
+def getFactOrDefault(key: str, facts: Dict[str, float], defaults: Dict[str, float], depth: int) -> float:
     if key in facts:
-        print(f"{key} = {facts[key]}")
+        verboseprint(f"{'  ' * depth}{key} = {facts[key]}")
         return facts[key]
     if key in defaults:
-        print(f"{key} = {defaults[key]} (default)")
+        verboseprint(f"{'  ' * depth}{key} = {defaults[key]} (default)")
         return defaults[key]
     raise Exception(f"No default found for '{key}'")
 
 
-def getCorporateEmissions(facts: Dict[str, float], defaults: Dict[str, float]) -> float:
+def getCorporateEmissions(facts: Dict[str, float], defaults: Dict[str, float], depth: int) -> float:
     if "corporate emissions mt per month" in facts:
-        return getFactOrDefault("corporate emissions mt per month", facts, defaults)
+        return getFactOrDefault("corporate emissions mt per month", facts, defaults, depth)
     if not "employees" in facts:
         raise Exception("Must provide either 'corporate emissions mt per month' or 'employees'")
     officeEmissionsPerEmployee = getFactOrDefault(
-        "office emissions mt per employee per month", facts, defaults
+        "office emissions mt per employee per month", facts, defaults, depth - 1
     )
     travelEmissionsPerEmployee = getFactOrDefault(
-        "travel emissions mt per employee per month", facts, defaults
+        "travel emissions mt per employee per month", facts, defaults, depth - 1
     )
-    itEmissionsPerEmployee = getFactOrDefault("it emissions mt per employee per month", facts, defaults)
+    itEmissionsPerEmployee = getFactOrDefault(
+        "it emissions mt per employee per month", facts, defaults, depth - 1
+    )
     commutingEmissionsPerEmployee = getFactOrDefault(
-        "commuting emissions mt per employee per month", facts, defaults
+        "commuting emissions mt per employee per month", facts, defaults, depth - 1
     )
-    corporateEmissions = getFactOrDefault('employees', facts, defaults) * (
+    corporateEmissions = getFactOrDefault("employees", facts, defaults, depth - 1) * (
         officeEmissionsPerEmployee
         + travelEmissionsPerEmployee
         + commutingEmissionsPerEmployee
         + itEmissionsPerEmployee
     )
-    print(f"corporate emissions mt per month: {corporateEmissions:.2f} (calculation)")
+    verboseprint(f"{'  ' * (depth - 1)}-------------------------------------------")
+    verboseprint(f"{'  ' * depth}corporate emissions mt per month: {corporateEmissions:.2f} (calculation)")
     return corporateEmissions
 
 
-def getCorporateEmissionsPerBidRequest(facts: Dict[str, float], defaults: Dict[str, float]) -> float:
-    corporateEmissionsMT = getCorporateEmissions(facts, defaults)
-    bidRequests = getFactOrDefault("bid requests processed billion per month", facts, defaults)
-    return corporateEmissionsMT / bidRequests
+def getCorporateEmissionsPerBidRequest(
+    facts: Dict[str, float], defaults: Dict[str, float], depth: int
+) -> float:
+    corporateEmissionsMT = getCorporateEmissions(facts, defaults, depth - 1)
+    bidRequests = getFactOrDefault("bid requests processed billion per month", facts, defaults, depth - 1)
+    corporateEmissionsPerBidRequest = corporateEmissionsMT / bidRequests
+    verboseprint(f"{'  ' * (depth - 1)}-------------------------------------------")
+    verboseprint(
+        f"{'  ' * depth}corporate emissions mt per billion bid requests: {corporateEmissionsPerBidRequest:.4f} (calculation)"
+    )
+    return corporateEmissionsPerBidRequest
 
 
-corporateEmissionsPerBidRequest = getCorporateEmissionsPerBidRequest(facts, defaults)
-print(f"corporate emissions g per bid request: {corporateEmissionsPerBidRequest:.4f}")
+depth = 3 if args.verbose else 0
+corporateEmissionsPerBidRequest = getCorporateEmissionsPerBidRequest(facts, defaults, depth - 1)
+verboseprint(f"{'  ' * (depth - 1)}-------------------------------------------")
+print(f"{'  ' * depth}corporate emissions g per bid request: {corporateEmissionsPerBidRequest/1000:.6f}")
