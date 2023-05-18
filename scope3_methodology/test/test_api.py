ca58c833bd4664a4904fbf2a032f2e40c53ef807
Niki Banerjee <nbanerjee@scope3.com>
2023-05-19 08:59:49 +1000

    Fixed defaults

--- scope3_methodology/test/test_api.py
+++ scope3_methodology/test/test_api.py
@@ -77,7 +77,7 @@ class TestAPI(unittest.TestCase):
         )
         self.assertEqual(
             organization_defaults[OrganizationType.PUBLISHER].revenue_allocation_to_digital_ads_pct,
-            Decimal("11.700000000"),
+            Decimal("10.933333333"),
         )
         self.assertTrue(organization_defaults[OrganizationType.GENERIC])
         self.assertEqual(
