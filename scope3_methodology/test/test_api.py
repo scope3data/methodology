04ab6f76dde27dc2e11861997df9b5ba8f98d790
Brian O'Kelley <bokelley@scope3.com>
2024-02-28 11:32:43 -0500

    Add ATP vendor calls to ad format and creative (#161)
    * add ad platform calls to calcs
    
    * add ATP defaults; fix formatting
    
    * switch creative width/height to sizes
    
    * fix random tab
    
    * fix this silly unit test
    
    * update defaults
    
    * add geo for atp compute, switch to per gb for network usage and add legacy scope3 numbers
    
    * push defaults
    
    * add default audio bitrates
    
    * add default triggers to video player
    
    * fix tests
    
    * remove ad_format.is_primary_experience as it is defined by presence of rendered width and height

--- scope3_methodology/test/test_api.py
+++ scope3_methodology/test/test_api.py
@@ -238,7 +238,7 @@ class TestAPI(unittest.TestCase):
         )
 
         docs_defs = docs_defaults
-        self.assertEqual(len(docs_defs), 14)
+        self.assertEqual(len(docs_defs), 19)
 
     def test_get_all_con_networking_connection_device_fixed_defaults(self):
         """Test get_all_networking_connection_device_defaults returns expected output"""
