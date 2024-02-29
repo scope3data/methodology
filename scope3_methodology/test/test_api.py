eacbc41a789a1cde627ae9c7d03de8e1e94a60f6
Brian O'Kelley <bokelley@scope3.com>
2024-02-28 20:40:57 -0500

    compute

--- scope3_methodology/test/test_api.py
+++ scope3_methodology/test/test_api.py
@@ -238,7 +238,7 @@ class TestAPI(unittest.TestCase):
         )
 
         docs_defs = docs_defaults
-        self.assertEqual(len(docs_defs), 19)
+        self.assertEqual(len(docs_defs), 23)
 
     def test_get_all_con_networking_connection_device_fixed_defaults(self):
         """Test get_all_networking_connection_device_defaults returns expected output"""
