a01027b34c088a1e5c731cfacffc0b011d5bbd1f
Pablo Gonzalez <pgonzalez@scope3.com>
2024-04-12 11:35:47 -0400

    updated number of params in test

--- scope3_methodology/test/test_api.py
+++ scope3_methodology/test/test_api.py
@@ -239,7 +239,7 @@ class TestAPI(unittest.TestCase):
         )
 
         docs_defs = docs_defaults
-        self.assertEqual(len(docs_defs), 37)
+        self.assertEqual(len(docs_defs), 32)
 
     def test_get_all_con_networking_connection_device_fixed_defaults(self):
         """Test get_all_networking_connection_device_defaults returns expected output"""
